1bcb06de1f1459c72352577af22f10d8225dcdab
RahulGupta77 <63935255+RahulGupta77@users.noreply.github.com>
2024-02-04 00:22:37 +0530

    initializing the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ backgroundColor: "yellow" }}>
+        <img src="./logo.png" alt="logo" height={60} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
