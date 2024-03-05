9a7e8b4029dffd931204c29a5ea6fea91b5a084c
RahulGupta77 <rg3498104@gmail.com>
2024-03-06 02:53:20 +0530

    Finished PriceFilter and RatingFilter UI logic

--- src/components/ProductsPage/PriceFilter.js
+++ src/components/ProductsPage/PriceFilter.js
@@ -1,5 +1,6 @@
 import { Typography } from "@material-tailwind/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import ReactSlider from "react-slider";
 import styled from "styled-components";
 
@@ -39,48 +40,166 @@ const StyledTrack = styled.div`
 const Track = (props, state) => <StyledTrack {...props} index={state.index} />;
 
 const PriceFilter = () => {
-  const [finalValue, setFinalValue] = useState([]);
+  const [finalValue, setFinalValue] = useState(null);
   const [currentValue, setCurrentValue] = useState([500, 7000]);
+  const [inputValue, setInputValue] = useState(["500", "7000"]);
+
+  const handleInputPrice = (e, idx) => {
+    const newArr = [...inputValue];
+    if (idx === 0) {
+      newArr[0] = e.target.value;
+    } else {
+      newArr[1] = e.target.value;
+    }
+    setInputValue(newArr);
+  };
+
+  const priceValidateHandler = (e, idx) => {
+    const minVal = Number(inputValue[0]);
+    const maxVal = Number(inputValue[1]);
+
+    if (idx === 0) {
+      let flag = false;
+      if (minVal < 500) {
+        toast.error("Minimum value cannot be less than 500₹");
+        flag = true;
+      } else if (minVal > 7000) {
+        toast.error("Invalid minimum value!!");
+        flag = true;
+      } else if (maxVal - minVal < 1500) {
+        toast.error("Maintain minimum difference of 1500₹");
+        flag = true;
+      }
+      if (flag) {
+        setInputValue((prev) => {
+          const res = [...prev];
+          res[0] = currentValue[0] + "";
+          return res;
+        });
+        return;
+      }
+      setCurrentValue((prev) => {
+        const res = [...prev];
+        res[0] = minVal;
+        return res;
+      });
+    } else {
+      let flag = false;
+      if (maxVal > 7000) {
+        toast.error("Maximum value cannot exceed 7000₹");
+        flag = true;
+      } else if (maxVal < 500) {
+        toast.error("Invalid maximum value!!");
+        flag = true;
+      } else if (maxVal - minVal < 1500) {
+        toast.error("Maintain minimum difference of 1500₹");
+        flag = true;
+      }
+      if (flag) {
+        setInputValue((prev) => {
+          const res = [...prev];
+          res[1] = currentValue[1] + "";
+          return res;
+        });
+        return;
+      }
+
+      setCurrentValue((prev) => {
+        const res = [...prev];
+        res[1] = maxVal;
+        return res;
+      });
+    }
+
+    if (minVal === 500 && maxVal === 7000) {
+      setFinalValue(null);
+      return;
+    }
+    setFinalValue([minVal, maxVal]);
+  };
+
+  const stringi = JSON.stringify(finalValue);
+
+  useEffect(() => {
+    if (finalValue) console.log("filtering");
+  }, [stringi, finalValue]);
+
+  const resetPriceValues = () => {
+    setFinalValue(null);
+    setCurrentValue([500, 7000]);
+    setInputValue(["500", "7000"]);
+  };
 
   return (
-    <div className="min-h-32 pt-8 pb-5 px-6 border-b-2">
-      <Typography
-        color="blue-gray"
-        className="font-extrabold uppercase text-sm"
-      >
-        Price
-      </Typography>
+    <div className="min-h-32 pt-8 pb-6 px-4 border-b-2">
+      <div className="flex justify-between">
+        <Typography
+          color="blue-gray"
+          className="font-extrabold uppercase text-sm px-2"
+        >
+          Price
+        </Typography>
+        {finalValue && (
+          <Typography
+            onClick={() => {
+              resetPriceValues();
+            }}
+            className="text-sm text-[#ed4f7a] px-2 uppercase font-extrabold cursor-pointer "
+          >
+            Clear
+          </Typography>
+        )}
+      </div>
 
-      <div className="mt-5 flex flex-col">
+      <div className="mt-8 flex flex-col">
         <StyledSlider
           defaultValue={[500, 7000]}
           minDistance={1500}
           max={7000}
           min={500}
-          value={currentValue}
-          onAfterChange={(val) => setFinalValue(val)}
-          onChange={(val) => setCurrentValue(val)}
+          value={[currentValue[0], currentValue[1]]}
+          onAfterChange={(val) => {
+            if (val[0] === 500 && val[1] === 7000) {
+              setFinalValue(null);
+              return;
+            }
+            setFinalValue(val);
+          }}
+          onChange={(val) => {
+            setCurrentValue(val);
+            setInputValue([val[0] + "", val[1] + ""]);
+          }}
           renderTrack={Track}
           renderThumb={Thumb}
         />
       </div>
 
-      <div className="mt-3 flex px-2 py-2 items-center justify-between">
+      <div className="mt-6 flex py-2 items-center text-[#263238] justify-between">
         <div>
-          <span className="text-sm font-bold mr-2">Min</span>
+          <span className="text-sm font-bold mr-2 ">Min ₹</span>
           <input
             type="number"
-            value={currentValue[0]}
-            className="placeholder-gray-500 text-sm font-semibold  bg-green-200 w-16 rounded-lg px-3 py-1 outline-none focus:shadow-outline "
+            value={inputValue[0]}
+            className="border-2 text-sm font-semibold  w-16 rounded-lg px-3 py-1 focus:shadow-outline focus:outline-pink-300"
+            onChange={(e) => handleInputPrice(e, 0)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") e.target.blur();
+            }}
+            onBlur={(e) => priceValidateHandler(e, 0)}
           />
         </div>
         <div>-</div>
         <div>
-          <span className="text-sm font-bold mr-2">Max</span>
+          <span className="text-sm font-bold mr-2 ">Max ₹</span>
           <input
             type="number"
-            value={currentValue[1]}
-            className="placeholder-gray-500 text-sm font-semibold bg-green-200 w-16 rounded-lg px-3 py-1 outline-none focus:shadow-outline"
+            value={inputValue[1]}
+            className="border-2 text-sm font-semibold  w-16 rounded-lg px-3 py-1 focus:shadow-outline focus:outline-pink-300"
+            onChange={(e) => handleInputPrice(e, 1)}
+            onBlur={(e) => priceValidateHandler(e, 1)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") e.target.blur();
+            }}
           />
         </div>
       </div>
--- src/components/ProductsPage/RatingFilter.js
+++ src/components/ProductsPage/RatingFilter.js
@@ -1,8 +1,13 @@
 import { Rating, Typography } from "@material-tailwind/react";
-import React from "react";
+import React, { useState } from "react";
 
 const RatingFilter = () => {
   const ratings = [4, 3, 2, 1];
+  const [selectedRating, setSelectedRating] = useState(null);
+
+  const handleRatingClick = (rating) => {
+    setSelectedRating(rating);
+  };
 
   return (
     <div className="min-h-32 pt-8 pb-5 px-6 border-b-2">
@@ -16,8 +21,12 @@ const RatingFilter = () => {
       <div className="mt-5 flex flex-col">
         {ratings.map((rating) => (
           <div
-            className="flex my-1 items-center gap-1 cursor-pointer text-[#263228] hover:text-[#f8c145] "
+            className={
+              "flex my-1 items-center gap-1 cursor-pointer hover:text-[#eeba41] " +
+              (selectedRating === rating ? "text-[#eeba41]" : "text-[#263228]")
+            }
             key={rating + "up"}
+            onClick={() => handleRatingClick(rating)}
           >
             <Rating value={rating} readonly />
             <div className="font-bold text-sm">& Up</div>
